93f1bf966bab5173603d3412336eac550c83d443
icyiceQ <kjell.ita@outlook.com>
2021-06-30 15:31:48 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,10 +31,42 @@ resource "azurerm_resource_group" "rg" {
 }
 
 # Create a virtual network
-resource "azurerm_virtual_network" "vnet" {
+/* resource "azurerm_virtual_network" "vnet" {
     name                = "myTFVnet"
     address_space       = ["10.10.10.0/24"]
     location            = "westeurope"
     resource_group_name = azurerm_resource_group.rg.name
-}
+} */
 
+# Create a Test Container Group
+resource "azurerm_container_group" "example" {
+  name                = "example-continst"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  ip_address_type     = "public"
+  dns_name_label      = "aci-label"
+  os_type             = "Linux"
+
+  container {
+    name   = "hello-world"
+    image  = "microsoft/aci-helloworld:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 443
+      protocol = "TCP"
+    }
+  }
+
+  container {
+    name   = "sidecar"
+    image  = "microsoft/aci-tutorial-sidecar"
+    cpu    = "0.5"
+    memory = "1.5"
+  }
+
+  tags = {
+    environment = "testing"
+  }
+}
