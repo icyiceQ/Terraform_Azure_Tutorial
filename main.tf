6efec9243a2371cba19006a89dd058775f348cb3
icyiceQ <kjell.ita@outlook.com>
2021-06-30 15:09:22 +0200

    Update main.tf
    new IP

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "azurerm_resource_group" "rg" {
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
     name                = "myTFVnet"
-    address_space       = ["10.0.10.0/24"]
+    address_space       = ["10.10.10.0/24"]
     location            = "westeurope"
     resource_group_name = azurerm_resource_group.rg.name
 }
