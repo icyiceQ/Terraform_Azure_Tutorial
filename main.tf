4afb02bda72e90234013052a4248792a9a0d9adf
icyiceQ <kjell.ita@outlook.com>
2021-06-30 15:47:45 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "azurerm_container_group" "TerraformTestKjell" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   ip_address_type     = "public"
-  dns_name_label      = "aci-label"
+  dns_name_label      = "terraformtestkjell.fit-ebike.ch"
   os_type             = "Linux"
 
   container {
