9f501f2b5335cf2975cf6d8fd2153e09b8be4cdb
icyiceQ <kjell.ita@outlook.com>
2021-06-30 15:45:31 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "azurerm_resource_group" "rg" {
 
 # Create a Test Container Group
 resource "azurerm_container_group" "TerraformTestKjell" {
-  name                = "TerraformTestKjell"
+  name                = "terraformtestkjell"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   ip_address_type     = "public"
